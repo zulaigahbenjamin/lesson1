579b6b95979cf4d4e6d195a0e28cc531b69d1bac
zulaigahbenjamin <130163047+zulaigahbenjamin@users.noreply.github.com>
2023-06-26 16:39:41 +0200

    com

---
+++ src/views/TutorialView.vue
@@ -0,0 +1,13 @@
+<!-- <template>
+<router-link :to="{name: 'Tutorial', params:{id: tutorial.id}}">
+<img- src="tutorial.image_url" :alt="tutorial.title">
+<h3>{{ tutorial.tital }}</h3>
+<p>Decription: {{ tutorial.desc }}</p>
+<p>Price: R{{ tutorial.price }}</p>
+</router-link>
+</template>
+<script>
+export default {
+    props: ["tutorial"]
+}
+</script>  -->
